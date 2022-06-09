7eab7b93342e329b5a4d1d74ed0709c3becdc30b
diegok23 <dkogut@gmail.com>
2022-06-10 00:35:21 +0200

    Completado hasta Level 5

--- server.js
+++ server.js
@@ -1,25 +1,128 @@
-const express = require("express");
-const cors = require("cors");
-
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const file = 'messages.json';
 const app = express();
+const bodyParser = require('body-parser'); //Añadido e instalado para leer desde el body
 
-app.use(cors());
+// AUX FUNCTIONS
+
+const myLogger = (req, res, next) => {
+  const log = {
+    date: new Date(),
+    url: req.url
+  };
+  console.log(JSON.stringify(log, null, 2));
+  next();
+};
+
+function getMessages() {
+  const text = fs.readFileSync(file);
+  return JSON.parse(text);
+}
+
+function saveMessages(arr) {
+  fs.writeFileSync(file, JSON.stringify(arr, null, 2));
+}
+
+/* DEPRECATED
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+  from: 'Bart',
+  text: 'Welcome to CYF chat system!'
 };
-
+let messages = [welcomeMessage];
+ */
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
-});
+const mainRoute = (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+};
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+const getMessagesAll = (req, res) => {
+  const messages = getMessages();
+  res.send(messages);
+};
+
+const getMessagesById = (req, res) => {
+  const messages = getMessages();
+  //const messages = getMessages();
+  const id = Number(req.params.messageId);
+  const message = messages.find((message) => message.id === id);
+  res.send(message);
+};
+
+// LEVEL 3.1
+const getMessageByQuery = (req, res) => {
+  const messages = getMessages();
+  queryWord = req.query.text.toLowerCase();
+  const search = messages.filter((message) => message.text.toLowerCase().includes(queryWord));
+  res.send(search);
+};
+
+// LEVEL 3.2
+const getMessagesLatest = (req, res) => {
+  const messages = getMessages();
+  const latest = messages.slice(-10);
+  res.send(latest);
+};
+
+// LEVELS 1 y 2
+const postMessage = (req, res) => {
+  const messages = getMessages();
+  if (req.body.text.length > 0 && req.body.from.length > 0) {
+    const newMessage = req.body;
+    newMessage.timeSent = new Date(); // LEVEL 4
+    newMessage.id = messages.length;
+    messages.push(newMessage);
+    saveMessages(messages);
+    res.send('Your message has been sent!');
+  } else {
+    res.status(400);
+    res.send('Your message has not been sent!');
+  }
+};
+
+// LEVEL 5
+const putMessageUpdate = (req, res) => {
+  const messages = getMessages();
+  const id = Number(req.params.messageId);
+  const message = messages.find((message) => message.id === id);
+  message.text = req.body.text;
+  message.from = req.body.from;
+  saveMessages(messages);
+  res.send('Your message has been modified!');
+};
+
+const deleteMessage = (req, res) => {
+  let messages = getMessages();
+  const id = Number(req.params.messageId);
+  const dmessage = messages.find((message) => message.id === id);
+  messages = messages.filter((message) => message.id !== id);
+  saveMessages(messages);
+  res.send('Your message has been deleted!');
+};
+
+//
+
+app.use(myLogger);
+app.use(express.json());
+app.use(bodyParser.json()); //Añadido e instalado para leer desde el body
+app.use(bodyParser.urlencoded({ extended: false })); //Añadido e instalado para leer desde el body
+app.use(cors());
+app.get('/', mainRoute);
+app.post('/messages', postMessage);
+app.put('/messages/:messageId', putMessageUpdate);
+app.get('/messages', getMessagesAll);
+app.get('/messages/search/', getMessageByQuery);
+app.get('/messages/latest', getMessagesLatest);
+app.get('/messages/:messageId', getMessagesById);
+app.delete('/messages/:messageId', deleteMessage);
+//
+
+const port = 3000;
+const url = `http://localhost:${port}/`;
+app.listen(port, () => console.log(`Listening on port ${url}`));
